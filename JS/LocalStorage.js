8364a2c5641b2fc2828bef1a3d84d70eb7843f36
Virendra Gayakwad <62784876+Viren-hub@users.noreply.github.com>
2021-04-23 11:30:34 +0530

    initial Commit :from Virendra
    this is related to adding Wedding page in project.

---
+++ JS/LocalStorage.js
@@ -0,0 +1,17 @@
+let data = localStorage.getItem("invite") ? JSON.parse(localStorage.getItem("invite")) : []
+
+function edit() {
+
+    let newData = {
+        "MaleName": document.getElementById("male").value,
+        "FemaleName": document.getElementById("female").value,
+        "venue": document.getElementById("place").value
+
+    }
+
+
+    data.push(newData);
+    localStorage.setItem("invite", JSON.stringify(data));
+    document.getElementById("Name1").innerHTML = newData.MaleName;
+    document.getElementById("Name2").innerHTML = newData.FemaleName;
+}
