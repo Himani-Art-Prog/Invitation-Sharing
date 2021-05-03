2a22aaf8205582abf9c4c8739cf6d60b8dd239f3
Virendra Gayakwad <62784876+Viren-hub@users.noreply.github.com>
2021-05-03 21:22:51 +0530

    Update BdyLocal.js

---
+++ JS/BdyLocal.js
@@ -0,0 +1,16 @@
+function editBirthDay() {
+
+    let newData = {
+        "BdayName": document.getElementById("name").value,
+        "date": document.getElementById("date").value,
+        "venue": document.getElementById("place").value
+
+    }
+
+
+    data.push(newData);
+    localStorage.setItem("Birthday", JSON.stringify(data));
+    document.getElementById("Name1").innerHTML = newData.BdayName;
+    document.getElementById("onDate").innerHTML = newData.date;
+    document.getElementById("city").innerHTML = newData.venue;
+}
